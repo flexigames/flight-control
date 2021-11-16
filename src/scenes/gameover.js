d835d5fa33d26dbb6dddb8197ce6c916c577e235
Finn Pauls <mbp@pauls.dev>
2021-11-16 16:37:33 +0100

    Actually allow any key to be pressed to restart

--- src/scenes/gameover.js
+++ src/scenes/gameover.js
@@ -1,20 +1,20 @@
 export default function gameover() {
   add([
-    text('game over'),
+    text("game over"),
     color(240, 240, 240),
     pos(center()),
-    origin('center'),
+    origin("center"),
     scale(2),
   ]);
 
   add([
     text(`press any key to continue`),
-    origin('bot'),
+    origin("bot"),
     color(240, 240, 240),
     pos(width() / 2, height() - 32),
   ]);
 
-  charInput(() => {
-    go('game');
+  onKeyPress(() => {
+    go("game");
   });
 }
