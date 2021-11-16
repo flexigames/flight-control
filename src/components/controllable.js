d47db676175400d2d0a2e3ee6973b9e34f4c36e9
lexenson <alexander.assmann91@gmail.com>
2021-11-16 14:20:07 +0100

    Make only one plane controllable at a time

--- src/components/controllable.js
+++ src/components/controllable.js
@@ -2,7 +2,13 @@ export default function controllable(turnSpeed = 2) {
   return {
     id: 'controllable',
     require: ['rotate'],
+    active: false,
+    add(){
+      this.defaultColor = this.color
+    },
     update() {
+      if (!this.active) return
+
       if (isKeyDown('left')) {
         this.angle -= turnSpeed;
       }
@@ -11,5 +17,14 @@ export default function controllable(turnSpeed = 2) {
         this.angle += turnSpeed;
       }
     },
+    toggleActive() {
+      this.active = !this.active;
+
+      if (this.active) {
+        this.color = rgb(255, 255, 255);
+      } else {
+        this.color = this.defaultColor
+      }
+    }
   };
 }
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -1,10 +1,29 @@
 import createPlane from '../entities/plane';
 
 export default function game() {
-  createPlane(vec2(0, height() / 2), 0);
-  createPlane(vec2(width(), height() / 2), 180);
+  const planes = [
+    createPlane(vec2(0, height() / 2), 0),
+    createPlane(vec2(width(), height() / 2), 180),
+  ];
 
-  onCollide('plane', 'plane', () => {
-    go('gameover');
-  });
+  handlePlaneCrashes();
+  handlePlaneControl();
+
+  function handlePlaneCrashes() {
+    onCollide('plane', 'plane', () => {
+      go('gameover');
+    });
+  }
+
+  function handlePlaneControl() {
+    let activePlaneIndex = 0;
+
+    planes[activePlaneIndex].toggleActive();
+
+    onKeyPress('space', () => {
+      planes[activePlaneIndex].toggleActive();
+      activePlaneIndex = (activePlaneIndex + 1) % planes.length;
+      planes[activePlaneIndex].toggleActive();
+    });
+  }
 }
