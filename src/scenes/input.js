3be41d556809ef57a88ca188ade77cbc5b1863ce
Finn Pauls <mbp@pauls.dev>
2021-11-16 23:21:42 +0100

    Messily trying to improve speech recognition
    Used some code from this blog article:
    https://bensonruan.com/voice-to-text-with-chrome-web-speech-api/
    
    The speech recognition there seemed pretty good, so I was hoping that
    it would improve it.

--- src/scenes/input.js
+++ src/scenes/input.js
@@ -1,18 +1,12 @@
 export default function createInput() {
-  return add([
+  let finalCommand = false;
+
+  const input = add([
     pos(30, height() - 20),
     text("command: "),
     {
       data: "",
       add() {
-        recognizeSpeech((text, isFinal) => {
-          this.text = "command: " + text;
-          if (isFinal) performCommand(text);
-        });
-        onCharInput((character) => {
-          this.data += character;
-          this.text = "command: " + this.data;
-        });
         onKeyRelease("enter", () => {
           performCommand(this.data);
           this.data = "";
@@ -25,6 +19,56 @@ export default function createInput() {
       },
     },
   ]);
+
+  const recognition = new webkitSpeechRecognition();
+
+  onKeyPress("space", () => {
+    finalCommand = false;
+    recognizeSpeech((final_transcript, interim_transcript, allFinal) => {
+      console.log(final_transcript, interim_transcript);
+      input.text = "command: " + final_transcript + interim_transcript;
+      if (allFinal) {
+        const test = performCommand(final_transcript + interim_transcript);
+        if (test.type === "error") {
+          input.text = "plane not found: " + test.planeName;
+        } else {
+          input.text = "executed: " + test.planeName + " " + test.degrees;
+        }
+      }
+    });
+  });
+
+  onKeyRelease("space", () => {
+    recognition.stop();
+    finalCommand = true;
+  });
+
+  function recognizeSpeech(onText) {
+    recognition.lang = "en-US";
+    recognition.interimResults = true;
+    recognition.continuous = true;
+
+    recognition.onresult = (event) => {
+      let interim_transcript = "";
+      let final_transcript = "";
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
+      }
+      const allFinal =
+        event.results.length > 0 &&
+        [...event.results].every((result) => result.isFinal);
+      onText(interim_transcript, final_transcript, allFinal);
+    };
+
+    recognition.start();
+    return recognition;
+  }
+
+  return input;
 }
 
 function performCommand(command) {
@@ -33,29 +77,15 @@ function performCommand(command) {
   console.log(command, planeName, degrees);
   const planes = get("plane");
   const plane = planes.find((plane) => plane.name === planeName);
-  if (!plane) return;
+
+  if (!plane) {
+    return {
+      type: "error",
+      planeName,
+    };
+  }
 
   plane.angle += Number(degrees);
-}
 
-function recognizeSpeech(onText) {
-  const recognition = new (window.SpeechRecognition ||
-    window.webkitSpeechRecognition ||
-    window.mozSpeechRecognition ||
-    window.msSpeechRecognition)();
-
-  recognition.lang = "en-US";
-  recognition.interimResults = false;
-  recognition.maxAlternatives = 1;
-  recognition.continuous = true;
-
-  recognition.onresult = (event) => {
-    if (event.results.length > 0) {
-      event.results[0].isFinal;
-      const result = event.results[event.results.length - 1];
-      onText(result[0].transcript, result.isFinal);
-    }
-  };
-
-  recognition.start();
+  return { degrees, planeName };
 }
